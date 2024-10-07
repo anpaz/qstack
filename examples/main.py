b4ddb80bba44d8de285c81b2bb6d803b69ce5208
Andres Paz <anpaz@cs.washington.edu>
2024-10-07 01:56:19 -0700

    main sample

--- examples/main.py
+++ examples/main.py
@@ -7,15 +7,15 @@ from qstack.layers.apps.instruction_set import *
 
 circuit = Circuit(
     [
-        PrepareZero([QubitId(0)]),
+        # PrepareZero([QubitId(0)]),
         PrepareOne([QubitId(1)]),
-        PrepareRandom([QubitId(2)]),
+        # PrepareRandom([QubitId(2)]),
         PrepareBell([QubitId(3), QubitId(4)]),
         Tick(),
         Comment("Measure qubits into classical registers"),
         Measure([RegisterId(0), QubitId(0)]),
         Measure([RegisterId(1), QubitId(1)]),
-        Measure([RegisterId(2), QubitId(2)]),
+        # Measure([RegisterId(2), QubitId(2)]),
         Measure([RegisterId(3), QubitId(3)]),
         Measure([RegisterId(4), QubitId(4)]),
     ]
@@ -53,21 +53,21 @@ backend.eval(t2).plot_histogram()
 from qstack.layers.apps.backend import Backend
 from qstack.noise import simple_noise_model
 
-noise_model = simple_noise_model(0.1)
+noise_model = simple_noise_model(0.02)
 backend = Backend(noise_model=noise_model)
 results = backend.eval(t1)
 results.plot_histogram()
 
 
-# %%
-results.get_histogram()
+# # %%
+# results.get_histogram()
 
 
 # %%
 from qstack.layers.repetition.compilers.apps.compiler import compile
 
 t3 = compile(t1)
-print(t3)
+# print(t3)
 
 # %%
 from qstack.layers.cliffords.backend import Backend
@@ -87,37 +87,43 @@ for i in list(hist.items())[:40]:
 from qstack.layers.cliffords.backend import Backend
 from qstack.noise import simple_noise_model
 
-noise_model = simple_noise_model(0.1)
+noise_model = simple_noise_model(0.02)
 backend = Backend(noise_model=noise_model)
 result = backend.eval(t3)
-# result.get_raw_histogram()
-
-
-# %%
 result.plot_histogram()
 
-# %%
-result.get_histogram()
-
-# %%
-decoder = t3.decoder
-
-# %%
-v = (False, False, 0, 0, 1, 1, 1, 1, 0, 0)
-v = (False, False, False, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0)
-v = (False, False, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1)
-v = (False, False, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0)
-decoder(v), v
-
-# %%
-
-# %%
-v = (False, False, 0, 0, 1, 1, 0, 1, 0, 0)
-decoder(v), v
+# # %%
+# result.get_histogram()
 
 
 # %%
-v = {data for data in result.raw_data if data[2:8] == [0, 0, 1, 1, 0, 1]}
+from qstack.layers.steane.compilers.apps.compiler import compile
+
+t4 = compile(t1)
+# print(t4)
+
 # %%
-v
+from qstack.layers.cliffords.backend import Backend
+
+backend = Backend()
+result = backend.eval(t4)
+result.plot_histogram()
+
+
+# %%
+hist = result.get_raw_histogram()
+for i in list(hist.items())[:40]:
+    print(i)
+
+
+# %%
+from qstack.layers.cliffords.backend import Backend
+from qstack.noise import simple_noise_model
+
+noise_model = simple_noise_model(0.02)
+backend = Backend(noise_model=noise_model)
+result = backend.eval(t4)
+result.plot_histogram()
+# result.get_raw_histogram()
+
 # %%
