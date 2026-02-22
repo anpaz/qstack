ea3fc4379c1dc72792142af38a305bc005da331c
Andres Paz <anpaz@cs.washington.edu>
2026-02-21 17:15:15 -0800

    remove old stuff

--- old.stuff/qstack.v0/compilers/apps/__init__.py
+++
@@ -1,6 +0,0 @@
-from ...layers.apps.compiler import known_instructions, compile
-
-__all__ = [
-    "known_instructions",
-    "compile",
-]
--- old.stuff/qstack.v0/layers/matrix/backends/pyQuil/__init__.py
+++
@@ -1,8 +0,0 @@
-import qstack.backend
-
-from .emulator import pyQuilEmulator as Emulator
-
-
-class Backend(qstack.backend.Backend):
-    def __init__(self) -> None:
-        super().__init__(Emulator())
