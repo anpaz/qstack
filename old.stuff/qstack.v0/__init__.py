e9525d0e185d74084f301456b0475d8eba6370d5
Andres Paz <anpaz@cs.washington.edu>
2026-05-09 23:01:33 -0700

    remove old.stuff

--- old.stuff/qstack.v0/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .stack import create_stack  # noqa F401
-
-from .handler import Handler
-from .gadget_definition import GadgetDefinition
--- old.stuff/qstack.v1/qpu.py
+++
@@ -1,9 +0,0 @@
-from qstack.gadget import Instruction
-
-
-class QPU:
-    def eval(Instruction) -> tuple[int] | None:
-        pass
-
-    def restart() -> None:
-        pass
